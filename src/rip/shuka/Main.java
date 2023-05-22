0675a103baf016d0af0f6a60d9ee806f3a6b8407
Lucas Bernard <lucas-fabien.bernard@regiocom.com>
2023-05-22 08:28:26 +0200

    Added Speed Test and changed argssssss

--- src/rip/shuka/Main.java
+++ src/rip/shuka/Main.java
@@ -4,23 +4,55 @@ import rip.shuka.core.utils.ErrorUtil;
 import rip.shuka.core.utils.FileReaderUtil;
 
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         FileReaderUtil fileReader = new FileReaderUtil();
 
-        if (args.length == 1) {
-            String file = args[0];
+        if (args.length >= 1) {
+            String file = null;
+            boolean speed_test = false;
+
+            Pattern pattern = Pattern.compile("=([^=]*)$");
+
+            for (String arg : args) {
+                Matcher matcher = pattern.matcher(arg);
+
+                if (arg.startsWith("file") && matcher.find()) {
+                    file = matcher.group(1);
+                }
+
+                if (arg.startsWith("speed_test") && matcher.find()) {
+                    speed_test = Boolean.parseBoolean(matcher.group(1));
+                }
+            }
+
+            if (file == null) { ErrorUtil.callError("Kein Pfad wurde ausgewählt"); }
 
             if (!file.endsWith(".uwu")) {
                 file = file + ".uwu";
+            }
+
+            if (speed_test) {
+                long startTime = System.nanoTime();
+
+                fileReader.read(file);
+
+                long endTime = System.nanoTime();
+
+                long executionTime = endTime - startTime;
+                double seconds = (double) executionTime / 1_000_000_000.0;
+
+                System.out.println("Execution time: " + seconds + " seconds");
+                return;
             }
 
             fileReader.read(file);
         } else {
             ErrorUtil.callError("Kein Pfad wurde ausgewählt");
         }
-
     }
 }
