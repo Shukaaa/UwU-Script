1f77b2f7ec5631470f97c139744ef1a09b47a2c7
Lucas Bernard <lucas-fabien.bernard@regiocom.com>
2023-05-23 13:40:21 +0200

    Added Sin, Cos and Tan

---
+++ src/rip/shuka/core/logic/functions/Math/Sin.java
@@ -0,0 +1,21 @@
+package rip.shuka.core.logic.functions.Math;
+
+import rip.shuka.core.logic.datatypes.Datatype;
+import rip.shuka.core.logic.datatypes.DatatypeObject;
+import rip.shuka.core.logic.datatypes.types.Float;
+import rip.shuka.core.logic.functions.Function;
+import rip.shuka.core.logic.parameter.Parameter;
+
+public class Sin extends Function {
+    public Sin() {
+        super("sin", new Parameter[]{
+                new Parameter(null, new Datatype[]{ new Float() }, "degree num")
+        });
+    }
+
+    public DatatypeObject execute(DatatypeObject[] args) {
+        double x = Double.parseDouble(args[0].value());
+
+        return new DatatypeObject(new Float(), Double.toString(Math.sin(x)));
+    }
+}
