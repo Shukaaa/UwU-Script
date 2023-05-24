5261f0550d157e095105bfe6a27c97cc7da3284f
Lucas Bernard <lucas-fabien.bernard@regiocom.com>
2023-05-24 12:04:07 +0200

    added comma escape sequence \c

--- src/rip/shuka/core/utils/StringCorrectorUtil.java
+++ src/rip/shuka/core/utils/StringCorrectorUtil.java
@@ -2,8 +2,12 @@ package rip.shuka.core.utils;
 
 public class StringCorrectorUtil {
     public static String correctForConsole(String str) {
-        // Basic Escape Sequences & \s for spaces
-        str = str.replace("\\t", "\t").replace("\\n", "\n").replace("\\s", " ");
+        // Basic Escape Sequences & \s for spaces & \c for command
+        str = str
+                .replace("\\t", "\t")
+                .replace("\\n", "\n")
+                .replace("\\s", " ")
+                .replace("\\c", ",");
 
         return str;
     }
